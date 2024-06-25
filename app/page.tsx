034341557b64d6298433752b01f12261bf7d058c
Joe Aguado <aguado.joe@gmail.com>
2024-06-25 14:55:08 -0500

    added: blog article

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
         Blog
       </h1>
       <p className="mb-4">
-        {`Hello World, my name is Joe Aguado and I'm a web developer based out of Dallas, Texas. I'm enthusiastic about technology that helps solve the needs for everyday business. The technological landscape is rapidly changing and I consider myself an asset to business owners who want to bridge those gaps. Check out some of my posts below to get an idea of the things I'm up to and some of my interests. Feel free to reach out to me at the links below!`}
+        {`Hello World, my name is Joe Aguado and I'm a web developer based out of Dallas, Texas. I'm enthusiastic about technology that helps solve the needs for everyday business. The technology landscape is rapidly changing and I consider myself an asset to business owners who want to bridge the gap between business and technology. Check out some of my posts below to get an idea of the things I'm up to and some of my interests. Feel free to reach out to me at the links below!`}
       </p>
       <div className="my-8">
         <BlogPosts />
