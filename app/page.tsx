a5b4b1bbfdfa9c1f79f6e030482331b249bf09a5
Joe Aguado <aguado.joe@gmail.com>
2024-07-03 12:43:07 -0500

    updated: hero image

--- app/page.tsx
+++ app/page.tsx
@@ -8,25 +8,24 @@ export default function Page() {
       <h1 className="mb-8 text-3xl font-semibold tracking-tighter">
         _blog
       </h1>
-      {/* <Image
-      // src={profile}
-      src="/images/profile.jpg"
-      className='rounded-[10px] border-2 border-gray-400 ml-4 mb-4'
+      <Image
+      src="/images/hero.svg"
+      className='rounded-tl-lg rounded-bl-lg rounded-br-lg mb-4'
       alt='profile image'
       priority={true}
       // placeholder='blur'
-      width={300}
-      height={450}
-      /> */}
+      width={600}
+      height={500}
+      />
 
-      <Image 
+      {/* <Image 
       src='https://github.com/joeoverflowcode/joeoverflowcode/blob/main/readme7.jpg?raw=true'
       alt='image'
       quality={100}
       width={540}
       height={420}
       className='border-2 border-gray-400 rounded-xl mb-4'
-      />
+      /> */}
       <p className="mb-4" style={{ textIndent: '24px' }}>
         {`Hello World, my name is Joe Aguado and I'm a web developer based out of Dallas, Texas. I'm enthusiastic about technology that helps solve the needs for everyday business. The technology landscape is rapidly changing and I consider myself an asset to business owners who want to bridge the gap between business and technology. Check out some of my posts below to get an idea of the things I'm up to and some of my interests.`}
       </p>
